2b9d4ca634d9821a923f6de37f8c8611e4b36752
박 상 민 <dhwkffks7@naver.com>
2022-07-09 22:08:01 +0900

    Initial commit

---
+++ app/src/main/java/com/sangmin/listviewpractice20220709/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sangmin.listviewpractice20220709
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
